a2b54c13a1c95ea680a58d0901edcc891a7e7e91
storytellerF <34095089+storytellerF@users.noreply.github.com>
2025-10-23 19:56:17 +0800

    install build tools

--- start-android.sh
+++ start-android.sh
@@ -18,11 +18,29 @@ shutdown() {
 
 AVD_NAME="android-35"
 SYS_IMG_PKG="system-images;android-35;google_apis;x86_64"
+BUILD_TOOLS_PKG="build-tools;35.0.0"
+PLATFORM_PKG="platforms;android-35"
+
 if ! sdkmanager --sdk_root=${ANDROID_SDK_ROOT} --list_installed | grep -q "${SYS_IMG_PKG}"; then
     echo "Installing system image (${SYS_IMG_PKG})..."
     sdkmanager --sdk_root=${ANDROID_SDK_ROOT} "${SYS_IMG_PKG}"
 else
     echo "System image (${SYS_IMG_PKG}) already installed."
+fi
+
+if ! sdkmanager --sdk_root=${ANDROID_SDK_ROOT} --list_installed | grep -q "${BUILD_TOOLS_PKG}"; then
+    echo "Installing build tools (${BUILD_TOOLS_PKG})..."
+    # 自动接受许可并安装
+   sdkmanager --sdk_root=${ANDROID_SDK_ROOT} "${BUILD_TOOLS_PKG}"
+else
+    echo "Build tools (${BUILD_TOOLS_PKG}) already installed."
+fi
+
+if ! sdkmanager --sdk_root=${ANDROID_SDK_ROOT} --list_installed | grep -q "${PLATFORM_PKG}"; then
+    echo "Installing platform (${PLATFORM_PKG})..."
+    sdkmanager --sdk_root=${ANDROID_SDK_ROOT} "${PLATFORM_PKG}"
+else
+    echo "Platform (${PLATFORM_PKG}) already installed."
 fi
 
 # Check if AVD exists
@@ -56,3 +74,4 @@ trap shutdown SIGTERM SIGINT
 
 # Wait for the emulator process to exit. The trap will interrupt this wait.
 wait "$EMULATOR_PID"
+echo "Emulator process has exited."
